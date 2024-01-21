f150276607f8144690e1ee5f6df4c19fc2c6ffa9
Sameer_Raturi <sameerraturi2001@gmail.com>
2024-01-21 15:35:38 +0530

    advance types

--- example-4-advance-types/src/app.ts
+++ example-4-advance-types/src/app.ts
@@ -69,3 +69,66 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+// discriminated Unions
+// we add the type to discriminate b/w the types in unions
+interface Horse {
+  // literal data type
+  type: "horse";
+  runningSpeed: number;
+}
+interface Bird {
+  // literal data type
+  type: "bird";
+  flyingSpeed: number;
+}
+
+type Animal = Horse | Bird;
+
+function moveAnimal(animal: Animal) {
+  let speed: number;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+    default:
+      speed = 0;
+      break;
+  }
+  console.log(`moving with the speed of ${speed}`);
+}
+
+// typecasting using as
+const userInput = document.getElementById("user-input")! as HTMLInputElement;
+
+interface ErrorContainer {
+  // index properties it will be use when we don't know the which and how many properties will can be added to an object
+  // but we know (in this example) the key should be of type string and the value should be of type string
+  [prop: string]: string;
+}
+
+const ErrorBag: ErrorContainer = {
+  username: "please enter correct name",
+  email: "please enter correct email id",
+};
+
+// we can do function overloading to show the return type of the function in typescript
+function add(n1: number, n2: number): number;
+function add(n1: string, n2: string): string;
+function add(n1: string, n2: number): string;
+function add(n1: number, n2: string): string;
+function add(n1: Combinable, n2: Combinable) {
+  if (typeof n1 === "string" || typeof n2 === "string") {
+    return n1.toString() + n2.toString();
+  }
+  return n1 + n2;
+}
+
+const result = add("sameer", " raturi");
+const splittedResult = result.split(" ");
+
+console.log(splittedResult);
